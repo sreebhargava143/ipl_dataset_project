fc8ace17cde8850c325adffff3285d2ae90fa7ab
SRI BHARGAVA YADAVALLI <bhargava.yadavalli@mountblue.io>
2019-09-05 18:31:33 +0530

    add total_matches_won.py

---
+++ total_matches_won.py
@@ -0,0 +1,42 @@
+from matplotlib import pyplot as plotter
+from csv import DictReader as reader
+from ipl_helper import *
+import random
+
+def extract_matches(csv_file):
+    matches_data = []
+    with open(csv_file) as matches_csv:
+        matches = reader(matches_csv)
+        for match in matches:
+            matches_data.append(match)
+    return matches_data
+
+def plot_matches_won(matches_won):
+    bottom = [0]*10
+    color_set = set()
+    color = '#012345'
+    color_set.add(color)
+    plotter.figure(figsize=(20,20))
+    plotter.style.use("fivethirtyeight")
+    for team in matches_won:
+        x_axis = list(matches_won[team].keys())
+        y_axis = list(matches_won[team].values())
+        plotter.bar(x_axis,y_axis, label = team, bottom = bottom, color = color)
+        bottom = [(x+y) for x,y in zip(y_axis,bottom)]
+        while color in color_set:
+            color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+        color_set.add(color)
+    plotter.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plotter.tight_layout()
+    plotter.show()
+
+def find_matches_won(csv_file):
+    matches = extract_matches(csv_file)
+    matches_won = get_team_season_dict(matches)
+    for match in matches:
+        if  match['winner'] == '':
+            continue
+        matches_won[match['winner']][match['season']] += 1
+    plot_matches_won(matches_won)
+
+find_matches_won('matches.csv')
