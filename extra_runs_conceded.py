c4c8f36577af472d8de48c039c332de5d9c0d7a0
SRI BHARGAVA YADAVALLI <bhargava.yadavalli@mountblue.io>
2019-09-05 21:29:56 +0530

    final commit for 4 parts

--- extra_runs_conceded.py
+++ extra_runs_conceded.py
@@ -1,6 +1,5 @@
 from ipl_helper import *
 from matplotlib import pyplot as plotter
-from csv import DictReader as reader
 def plot_extras_conceded(year,extras_conceded):
     plotter.barh(list(extras_conceded.keys()),list(extras_conceded.values()))
     plotter.style.use("fivethirtyeight")
@@ -10,16 +9,15 @@ def plot_extras_conceded(year,extras_conceded):
     plotter.show()
 
 def find_extra_runs_conceded(year, matches_csv, deliveries_csv):
-    matches = extract_list(matches_csv)
-    id_set = get_id_set(year, matches)
+    id_set = extract_id_set(year, matches_csv)
     extra_runs_conceded = {}
-    deliveries = extract_deliveries(deliveries_csv)
+    deliveries = extract_deliveries(deliveries_csv, id_set)
     for match_id in id_set:
         for delivery in deliveries[match_id]:
                 if delivery['bowling_team'] not in extra_runs_conceded:
                     extra_runs_conceded[delivery['bowling_team']] = 0
                 extra_runs_conceded[delivery['bowling_team']] += int(delivery['extra_runs'])
-
-    plot_extras_conceded(year,extra_runs_conceded) 
+    print(sorted_dict(extra_runs_conceded,by = 'value'))
+    plot_extras_conceded(year,sorted_dict(extra_runs_conceded,by = 'value')) 
 
 find_extra_runs_conceded(2016, 'matches.csv', 'deliveries.csv')
--- ipl_helper.py
+++ ipl_helper.py
@@ -1,4 +1,5 @@
 from csv import DictReader as reader
+import collections
 
 def get_seasons_dict(matches):
     total_seasons = set()
@@ -26,10 +27,14 @@ def get_id_set(year,matches):
             id_set.add(match['id'])
     return sorted(id_set)
 
-def sorted_dict(dictionary,reverse = False):
+def sorted_dict(dictionary,reverse = False, by = 'key'):
     sorted_dict = {}
-    for key in sorted(dictionary,reverse = reverse) :
-        sorted_dict[key] = dictionary[key]
+    if(by=='key'):
+        sorted_dict = sorted(dictionary.items(), key=lambda entry:entry[0], reverse=reverse)
+        sorted_dict = collections.OrderedDict(sorted_dict)
+    if(by=='value'):
+        sorted_dict = sorted(dictionary.items(), key=lambda entry:entry[1], reverse=reverse)
+        sorted_dict = collections.OrderedDict(sorted_dict)
     return sorted_dict
 
 def extract_list(csv_file):
@@ -40,14 +45,25 @@ def extract_list(csv_file):
             matches_data.append(match)
     return matches_data
 
-def extract_deliveries(csv_file):
+def extract_id_set(year, csv_file):
+    id_set = set()
+    with open(csv_file) as matches_csv:
+        matches = reader(matches_csv)
+        for match in matches:
+            if year==int(match['season']):        
+                id_set.add(match['id'])
+    return sorted(id_set)
+
+
+def extract_deliveries(csv_file, id_set):
     deliveries_dict = {}
     with open(csv_file) as deliveriescsv:
         deliveries = reader(deliveriescsv)
         for delivery in deliveries:
-            if delivery['match_id'] not in deliveries_dict:
-                deliveries_dict[delivery['match_id']] = []
-            deliveries_dict[delivery['match_id']].append(delivery)
+            if delivery['match_id'] in id_set:
+                if delivery['match_id'] not in deliveries_dict:
+                    deliveries_dict[delivery['match_id']] = []
+                deliveries_dict[delivery['match_id']].append(delivery)
     return deliveries_dict
 
 
