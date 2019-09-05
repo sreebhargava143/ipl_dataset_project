61c79f2fe5b04459f7e72c347b3e51fe7759e71a
SRI BHARGAVA YADAVALLI <bhargava.yadavalli@mountblue.io>
2019-09-05 17:48:09 +0530

    add ipl_helper.py and matches_played_per_year.py

---
+++ ipl_helper.py
@@ -0,0 +1,37 @@
+
+
+def get_seasons_dict(matches):
+    total_seasons = set()
+    seasons = {}
+    for match in matches:
+        total_seasons.add(match['season'])
+    for season in sorted(total_seasons):
+        seasons[season] = 0
+    return seasons
+
+def get_team_season_dict(matches):
+    seasons = get_seasons_dict(matches)
+    total_teams = {}
+    for match in matches:
+        print(match)
+        break
+        if match['team1'] not in total_teams:
+            total_teams[match['team1']] = seasons.copy()
+    # print(total_teams)
+    return total_teams
+
+
+def get_id_set(year,matches):
+    id_set = set()
+    for match in matches:
+        if year==int(match['season']):        
+            id_set.add(match['id'])
+    return id_set
+
+def sorted_dict(dictionary,reverse = False):
+    sorted_dict = {}
+    for key in sorted(dictionary,reverse = reverse) :
+        sorted_dict[key] = dictionary[key]
+    return sorted_dict
+
+
---
+++ matches_played_per_year.py
@@ -0,0 +1,28 @@
+from matplotlib import pyplot as plotter
+from csv import DictReader as reader
+from ipl_helper import *
+
+
+def plot_matches_played(year_match_map):
+    plotter.style.use("fivethirtyeight")
+    plotter.bar(list(year_match_map.keys()),list(year_match_map.values()))
+    plotter.grid()
+    plotter.xlabel('Years')
+    plotter.ylabel('Matches Played')
+    plotter.title('IPL Matches Played Per Year')
+    plotter.tight_layout()
+    plotter.legend()
+    plotter.show()
+
+def find_matches_per_year(csvfile):
+    yearMatchMap = {}
+    with open(csvfile) as matches_csv:
+        matches = reader(matches_csv)
+        for match in matches:
+            if match['season'] not in yearMatchMap:
+                yearMatchMap[match['season']] = 1
+            else:
+                yearMatchMap[match['season']] +=1
+    plot_matches_played(sorted_dict(yearMatchMap))
+
+find_matches_per_year('matches.csv')
